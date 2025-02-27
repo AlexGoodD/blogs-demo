46f61215ec6ad538c597ba5d9469a4813f0e2cb7
AlexGoodD <alejandrochavarb@gmail.com>
2025-02-27 14:11:56 -0600

    Test de edición y validación de cambios implementado

--- tests/browser/pages/posts/list_posts.spec.ts
+++ tests/browser/pages/posts/list_posts.spec.ts
@@ -30,5 +30,27 @@ test.group('Posts page', (group) => {
     const postCount = await page.countPosts() //Cuenta el número de post
     // Then
     assert.equal(postCount, 4) //Verifica que el número de post sea 4
+  })
+
+  test('edit posts', async ({ assert, browserContext, visit }) => {
+    //(Actualmente tiene que entrar al post para poder verificar el cambio, mejorar eso)
+    // Given
+    const user = await UserFactory.create() //genera usuario 'random'
+    const posts = await PostFactory.createMany(5) //genera 5 posts 'random'
+    await browserContext.loginAs(user) //Loguea al usuario
+    const page = await visit(PostsList) //Visita la página de posts
+    // When
+
+    //Debe acceder al botón editar/read-more
+    await page.read(0)
+    //Acceder al campo de descripción/Modificar el campo de descripción
+    await page.page.getByLabel('Description').fill('Hola')
+    //Presionar el botón de create-post
+    await page.editPost()
+
+    // Then
+    //Verificar que el post tal, haya cambiado y ahora sea esto otro
+    await page.read(0)
+    await page.page.assertTextContains('Description', '\n  Hola\n')
   }).pin()
 })
--- tests/browser/pages/posts/pages/post_page.ts
+++ tests/browser/pages/posts/pages/post_page.ts
@@ -9,6 +9,16 @@ export class PostsList extends BasePage {
     await deleteButtons.nth(position).click()
   }
 
+  //Presionar botón leer más
+  async read(position: number) {
+    const readButtons = await this.page.getByRole('button', { name: 'Read' })
+    await readButtons.nth(position).click()
+  }
+
+  async editPost() {
+    await this.page.getByRole('button', { name: 'Create post' }).click()
+  }
+
   //Contar número de posts
   async countPosts() {
     const posts = await this.page.locator('div.grid > form') // Selecciona todos los formularios dentro de la cuadrícula
