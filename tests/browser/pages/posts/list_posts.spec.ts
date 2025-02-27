2a27d687f3d430dc3ac3d3e09220a5a04d0cbf1c
AlexGoodD <alejandrochavarb@gmail.com>
2025-02-27 11:54:29 -0600

    Test eliminación de post

--- tests/browser/pages/posts/list_posts.spec.ts
+++ tests/browser/pages/posts/list_posts.spec.ts
@@ -1,9 +1,10 @@
 import { test } from '@japa/runner'
 import User from '#models/user'
 import testUtils from '@adonisjs/core/services/test_utils'
-import { PostsList } from '#tests/browser/pages/posts/pages/PostsList'
+import { PostsList } from '#tests/browser/pages/posts/pages/post_page'
 import { UserFactory } from '#database/factories/user_factory'
 import { PostFactory } from '#database/factories/post_factory'
+import { count } from 'node:console'
 
 test.group('Posts page', (group) => {
   group.each.setup(() => testUtils.db().withGlobalTransaction())
@@ -14,7 +15,20 @@ test.group('Posts page', (group) => {
     await browserContext.loginAs(user)
     const page = await visit(PostsList)
     await page.page.pause()
-    // When∂
+    // When
     // Then
+  })
+
+  test('delete posts', async ({ assert, browserContext, visit }) => {
+    // Given
+    const user = await UserFactory.create() //genera usuario 'random'
+    const posts = await PostFactory.createMany(5) //genera 5 posts 'random'
+    await browserContext.loginAs(user) //Loguea al usuario
+    const page = await visit(PostsList) //Visita la página de posts
+    // When
+    await page.delete(0) //Elimina el post de la posición 0
+    const postCount = await page.countPosts() //Cuenta el número de post
+    // Then
+    assert.equal(postCount, 4) //Verifica que el número de post sea 4
   }).pin()
 })
