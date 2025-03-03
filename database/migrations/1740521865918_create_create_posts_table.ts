ca68b74a769280c115927a6553c701861558e5eb
AlexGoodD <alejandrochavarb@gmail.com>
2025-03-03 11:25:30 -0600

    Lógica de posts

--- database/migrations/1740521865918_create_create_posts_table.ts
+++ database/migrations/1740521865918_create_create_posts_table.ts
@@ -7,6 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('title')
+      table.string('userEmail')
       table.string('description')
       table.integer('user_id').unsigned().references('users.id').notNullable()
       table.timestamp('created_at')
